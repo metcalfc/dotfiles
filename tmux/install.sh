cb2c24cb151dae4539255ef310131407570973fb
Chad Metcalf <metcalfc@gmail.com>
2023-07-19 15:39:39 -0700

    Adding basic tmux.

---
+++ tmux/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+    brew install tmux
+fi
+
+# check if tmux plugin manager is installed and install if not
+if [ ! -d "${HOME}/.config/tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+fi
+
+# check if ~/.config/tmux.conf exists and create a symlink to the one in this repo if not
+if [ ! -f "${HOME}/.config/tmux.conf" ]; then
+    ln -s "${DOTFILES}/tmux/tmux.conf" "${HOME}/.config/tmux/tmux.conf"
+fi
+
+exit 0
