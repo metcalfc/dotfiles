f676dc7c195de2968d82cb3ea2f400d1f232b7b3
Chad Metcalf <metcalfc@gmail.com>
2023-07-20 14:12:25 -0700

    Deprecating the old vim setup.

---
+++ nvim/nvim.zsh
@@ -0,0 +1,5 @@
+alias packvim="nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'"
+alias vi=nvim
+alias vim=nvim
+export EDITOR=nvim
+
--- vim/vim.zsh
+++
@@ -1,3 +0,0 @@
-alias vi=nvim
-alias vim=nvim
-export EDITOR=nvim
