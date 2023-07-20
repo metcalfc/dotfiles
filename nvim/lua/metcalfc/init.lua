3b91a551c0fbe78eb696dc84aa6b08eeb5f7cb58
Chad Metcalf <metcalfc@gmail.com>
2023-07-20 13:55:30 -0700

    Adding a mapping to edit the plugins.

--- nvim/lua/metcalfc/init.lua
+++ nvim/lua/metcalfc/init.lua
@@ -71,7 +71,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/src/dotfiles/nvim/lua/metcalfc/packer.lua<CR>");
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/src/dotfiles/nvim/lua/metcalfc/plugins.lua<CR>");
 
 --vim.keymap.set("n", "<leader><leader>", function()
 --    vim.cmd("so")
