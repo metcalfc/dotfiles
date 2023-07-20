82b7112d679d3a14f9178182c415a3c4c8745161
Chad Metcalf <metcalfc@gmail.com>
2023-07-19 20:25:35 -0700

    Adding a keymap for format.

--- nvim/lua/metcalfc/init.lua
+++ nvim/lua/metcalfc/init.lua
@@ -61,7 +61,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
---vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
