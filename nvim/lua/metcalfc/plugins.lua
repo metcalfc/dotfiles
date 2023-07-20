b4d73671cc4755a6c7f88b5b29bb1ab910aaf479
Chad Metcalf <metcalfc@gmail.com>
2023-07-20 15:57:55 -0700

    Adding lualine.

--- nvim/lua/metcalfc/plugins.lua
+++ nvim/lua/metcalfc/plugins.lua
@@ -189,6 +189,18 @@ return packer.startup(function(use)
     })
 
     use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "nvim-tree/nvim-web-devicons", opt = true },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "catppuccin",
+                },
+            })
+        end,
+    })
+
+    use({
         "ellisonleao/glow.nvim",
         config = function()
             require("glow").setup()
